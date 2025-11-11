c799e30223ef8e8b1795d291054971f2e5f4f1cb
Diego Niquefa <niquefa.diego@gmail.com>
2025-11-10 21:44:38 -0800

    Implement focus points

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
 
 // ----- Utilities -----
 
+FILE *DebugFile = NULL;
 #define Debug(...)                       \
     {                                    \
         fprintf(DebugFile, __VA_ARGS__); \
@@ -17,16 +18,21 @@
 
 inline int Compare(double a, double b)
 {
-    return a + EPSILON < b ? -1 : (b + EPSILON < a ? 1 : 0);
+    static const double epsilon = 1e-9;
+    return a + epsilon < b ? -1 : (b + epsilon < a ? 1 : 0);
 }
-
-FILE *DebugFile = NULL;
 
 // Dynamic array macros
 #define ListSize(da)               (da?((size_t*)da)[-1]:0)
 #define ListCapacity(da)           (da?((size_t*)da)[-2]:0)
-#define ListFree(da)               {if(da) free((size_t*)da - 2); da = NULL;}
 #define ListPop(da)                da[--(((size_t*)da)[-1])]
+
+#define ListFree(da)                                                           \
+        do {                                                                   \
+            if(da) free((size_t*)da - 2);                                      \
+            da = NULL;                                                         \
+        } while(0)
+
 #define ListPush(da, value) {                                                  \
         if(!da) {                                                              \
             size_t* _ptr = malloc(2 * sizeof(size_t) + 4 * sizeof(*(da)));     \
@@ -44,7 +50,7 @@ FILE *DebugFile = NULL;
         da[((size_t*)da)[-1]++] = value;                                       \
     }
 
-// ----- Core state -----
+/// ----- Geometry -----
 
 typedef struct
 {
@@ -52,7 +58,69 @@ typedef struct
     double y;
 } Point;
 
-typedef struct {
+inline Point Add(const Point a, const Point b)
+{
+    return (Point){.x = a.x + b.x, .y = a.y + b.y};
+}
+
+inline Point Subtract(const Point a, const Point b)
+{
+    return (Point){.x = a.x - b.x, .y = a.y - b.y};
+}
+
+inline Point Scale(const Point a, double s)
+{
+    return (Point){.x = a.x * s, .y = a.y * s};
+}
+
+inline double Cross(const Point a, const Point b)
+{
+    return a.x * b.y - a.y * b.x;
+}
+
+inline double Cross3(const Point fr, const Point toA, const Point toB)
+{
+    return Cross(Subtract(toA, fr), Subtract(toB, fr));
+}
+
+inline int Sign(const double v)
+{
+    return (v > 0) - (v < 0);
+}
+
+// Return true if the line ab intersects segment cd in a point different
+// than c or d.
+bool LineCutsSegment(Point a, Point b, Point c, Point d)
+{
+    int signC = Sign(Cross3(a, b, c));
+    int signD = Sign(Cross3(a, b, d));
+    return signC + signD == 0 && signC != signD;
+}
+
+// Returns true if segments ab and cd intersect in one point different from
+// their endpoints. If true, *outputPoint will be assigned the intersection.
+bool TryFindSegmentInnerIntersection(
+    Point a, Point b, Point c, Point d, Point* outPoint)
+{
+    if (!LineCutsSegment(a, b, c, d)) return false;
+    if (!LineCutsSegment(c, d, a, b)) return false;
+
+    Point da = Subtract(b, a);
+    Point dc = Subtract(d, c);
+    // a + da*t = c + dc*s // t and s are some scalars
+    // (a + da*t) x dc = (c + dc*s) x dc // x is cross product
+    // a x dc + (da x dc) * t = c x dc // distribute and (dc x dc) = 0
+    // t = (c x dc - a x dc) / (da x dc) // isolate t
+    // t = ((c - a) x dc) / (da x dc) // cross product is distributive
+    double t = Cross(Subtract(c, a), dc) / Cross(da, dc);
+    *outPoint = Add(a, Scale(da, t));
+    return true;
+}
+
+// ----- Core state -----
+
+typedef struct
+{
     size_t a;
     size_t b;
 } LineSegment;
@@ -65,7 +133,8 @@ typedef struct
 
 CoreStateSingleton CoreState = {0};
 
-size_t FindPoint(Point p) {
+size_t FindPoint(Point p)
+{
     size_t nPoints = ListSize(CoreState.points);
     for (size_t i = 0; i < nPoints; i++) {
         if (Compare(CoreState.points[i].x, p.x) == 0 &&
@@ -76,25 +145,56 @@ size_t FindPoint(Point p) {
     return SIZE_MAX;
 }
 
-size_t AddPoint(Point p) {
+size_t AddPoint(Point p)
+{
     size_t index = FindPoint(p);
     if (index != SIZE_MAX) return (size_t)index;
     ListPush(CoreState.points, p);
     return ListSize(CoreState.points) - 1;
 }
 
-void AddSegment(LineSegment s) {
-    ListPush(CoreState.segments, s);
+void AddSegment(LineSegment s)
+{
+    #define segmentList CoreState.segments
+    #define pointList CoreState.points
+    for (size_t i = 0; i < ListSize(segmentList); i++) {
+        Point p;
+        bool foundIntersection = TryFindSegmentInnerIntersection(
+            pointList[s.a], pointList[s.b],
+            pointList[segmentList[i].a], pointList[segmentList[i].b],
+            &p);
+            if (foundIntersection) {
+                Debug("Found segment intersection at (%f, %f)", p.x, p.y);
+                AddPoint(p);
+            }
+        }
+    ListPush(segmentList, s);
+    #undef segmentList
+    #undef pointList
 }
 
 void InitializeCoreState()
 {
-    size_t a = AddPoint((Point){.x= -5.0, .y=-8.0});
-    size_t b = AddPoint((Point){.x= 8.0,  .y=13.0});
-    size_t c = AddPoint((Point){.x= 5.0,  .y=5.0 });
-    size_t d = AddPoint((Point){.x= 5.0,  .y=10.0});
-    AddSegment((LineSegment){.a=a, .b=b});
-    AddSegment((LineSegment){.a=c, .b=d});
+    AddSegment((LineSegment){
+        AddPoint((Point){.x= -5.0, .y=-8.0}),
+        AddPoint((Point){.x= 8.0,  .y=13.0})
+    });
+    AddSegment((LineSegment){
+        AddPoint((Point){.x= 5.0,  .y=5.0 }),
+        AddPoint((Point){.x= 5.0,  .y=10.0})
+    });
+    AddSegment((LineSegment){
+        AddPoint((Point){.x= -1.5,  .y=5.3 }),
+        AddPoint((Point){.x= 6.5,  .y=-4.7})
+    });
+    AddSegment((LineSegment){
+        AddPoint((Point){.x= -1,  .y=-1 }),
+        AddPoint((Point){.x= -2,  .y=-2})
+    });
+    AddSegment((LineSegment){
+        AddPoint((Point){.x= -1,  .y=-2 }),
+        AddPoint((Point){.x= -2,  .y=-1})
+    });
 }
 
 void FreeCoreStateSingleton()
@@ -110,6 +210,16 @@ typedef struct
     Vector2 fr; // a.x <= b.x and a.y <= b.y
     Vector2 to;
 } Rect;
+
+inline float RectWidth(Rect r)
+{
+    return r.to.x - r.fr.x;
+}
+
+inline float RectHeight(Rect r)
+{
+    return r.to.y - r.fr.y;
+}
 
 typedef struct
 {
@@ -127,7 +237,8 @@ typedef struct
 WindowStateSingleton WindowState = {0};
 ViewStateSingleton ViewState = {0};
 
-typedef struct {
+typedef struct
+{
     int minFps;
     int maxFps;
     double avgFps;
@@ -149,37 +260,41 @@ void SetScreenSize(int width, int height)
 // Get screen position given cartesian plane position
 Vector2 ToVector2(Point p)
 {
+    const Rect viewRect = ViewState.viewPlaneRect;
+    const Rect canvasRect = WindowState.canvasRect;
     Vector2 r = {
-        .x = Normalize(p.x, ViewState.viewPlaneRect.fr.x, ViewState.viewPlaneRect.to.x),
-        .y = Normalize(p.y, ViewState.viewPlaneRect.to.y, ViewState.viewPlaneRect.fr.y)
+        .x = Normalize(p.x, viewRect.fr.x, viewRect.to.x),
+        .y = Normalize(p.y, viewRect.to.y, viewRect.fr.y)
     };
-    r.x = Lerp(WindowState.canvasRect.fr.x, WindowState.canvasRect.to.x, r.x);
-    r.y = Lerp(WindowState.canvasRect.fr.y, WindowState.canvasRect.to.y, r.y);
+    r.x = Lerp(canvasRect.fr.x, canvasRect.to.x, r.x);
+    r.y = Lerp(canvasRect.fr.y, canvasRect.to.y, r.y);
     return r;
 }
 
 // Get cartesian plane position given screen position
 Point ToPoint(Vector2 p)
 {
+    const Rect viewRect = ViewState.viewPlaneRect;
+    const Rect canvasRect = WindowState.canvasRect;
     Point r = {
-        .x = Normalize(p.x, WindowState.canvasRect.fr.x, WindowState.canvasRect.to.x),
-        .y = Normalize(p.y, WindowState.canvasRect.fr.y, WindowState.canvasRect.to.y)
+        .x = Normalize(p.x, canvasRect.fr.x, canvasRect.to.x),
+        .y = Normalize(p.y, canvasRect.fr.y, canvasRect.to.y)
     };
-    r.x = Lerp(ViewState.viewPlaneRect.fr.x, ViewState.viewPlaneRect.to.x, r.x);
-    r.y = Lerp(ViewState.viewPlaneRect.to.y, ViewState.viewPlaneRect.fr.y, r.y);
+    r.x = Lerp(viewRect.fr.x, viewRect.to.x, r.x);
+    r.y = Lerp(viewRect.to.y, viewRect.fr.y, r.y);
     return r;
 }
 
 float XScreenToPlaneRatio()
 {
-    return (WindowState.canvasRect.to.x - WindowState.canvasRect.fr.x)
-           / (ViewState.viewPlaneRect.to.x - ViewState.viewPlaneRect.fr.x);
+    return RectWidth(WindowState.canvasRect)
+            / RectWidth(ViewState.viewPlaneRect);
 }
 
 float YScreenToPlaneRatio()
 {
-    return (WindowState.canvasRect.to.y - WindowState.canvasRect.fr.y)
-           / (ViewState.viewPlaneRect.to.y - ViewState.viewPlaneRect.fr.y);
+    return RectHeight(WindowState.canvasRect)
+            / RectHeight(ViewState.viewPlaneRect);
 }
 
 float FindAxisTickSpacing(float size)
@@ -205,77 +320,121 @@ void RenderCartesianPlaneAxes()
     // Calculate tick spacing, size and text height
     float spacingY, tickSizeY, spacingX, tickSizeX;
     int textHeight = 0;
-    bool isXAxisVisible = ViewState.viewPlaneRect.fr.y <= 0.0 && ViewState.viewPlaneRect.to.y >= 0.0;
-    bool isYAxisVisible = ViewState.viewPlaneRect.fr.x <= 0.0 && ViewState.viewPlaneRect.to.x >= 0.0;
+    const Rect viewRect = ViewState.viewPlaneRect;
+    bool isXAxisVisible = viewRect.fr.y <= 0.0 && viewRect.to.y >= 0.0;
+    bool isYAxisVisible = viewRect.fr.x <= 0.0 && viewRect.to.x >= 0.0;
     if (isXAxisVisible) {
-        spacingX = FindAxisTickSpacing(ViewState.viewPlaneRect.to.x - ViewState.viewPlaneRect.fr.x);
+        spacingX = FindAxisTickSpacing(RectWidth(viewRect));
         tickSizeX = spacingX * XScreenToPlaneRatio() * TICK_SIZE_FACTOR;
         textHeight = (int)tickSizeX * 2;
     }
     if (isYAxisVisible) {
-        spacingY = FindAxisTickSpacing(ViewState.viewPlaneRect.to.y - ViewState.viewPlaneRect.fr.y);
+        spacingY = FindAxisTickSpacing(RectHeight(viewRect));
         tickSizeY = spacingY * YScreenToPlaneRatio() * TICK_SIZE_FACTOR;
         if (textHeight == 0 || textHeight > (int)tickSizeY * 2)
             textHeight = (int)tickSizeY * 2;
     }
 
-    // Draw X axis
-    if (isXAxisVisible)
-    {
-        Vector2 a = ToVector2((Point){ViewState.viewPlaneRect.fr.x, 0.0});
-        Vector2 b = ToVector2((Point){ViewState.viewPlaneRect.to.x, 0.0});
+    if (isXAxisVisible) { // Draw X axis
+        Vector2 a = ToVector2((Point){viewRect.fr.x, 0.0});
+        Vector2 b = ToVector2((Point){viewRect.to.x, 0.0});
         DrawLineV(a, b, axisColor);
-        
-        float firstTick = ceilf(ViewState.viewPlaneRect.fr.x / spacingX) * spacingX;
-        for (float tick = firstTick; tick <= ViewState.viewPlaneRect.to.x; tick += spacingX)
-        {
-            if (Compare(tick, 0.0f) == 0)
-                continue;
-
+        float firstTick = ceilf(viewRect.fr.x / spacingX) * spacingX;
+        for (float tick = firstTick; tick <= viewRect.to.x; tick += spacingX) {
+            if (Compare(tick, 0.0f) == 0) continue;
+            // Draw the tick
             Vector2 tickPos = ToVector2((Point){tick, 0.0});
-            DrawLine(tickPos.x, tickPos.y - tickSizeX, tickPos.x, tickPos.y + tickSizeX, axisColor); // Draw the tick
+            DrawLine(
+                tickPos.x, tickPos.y - tickSizeX,
+                tickPos.x, tickPos.y + tickSizeX,
+                axisColor);
+            // Draw the tick number
             snprintf(buffer, sizeof(buffer), "%.0f", tick);
             int textWidth = MeasureText(buffer, textHeight);
-            // Draw the tick number
-            DrawText(buffer, tickPos.x - 0.5f * textWidth, tickPos.y - textHeight - tickSizeX - 1, textHeight, axisNumberColor);
+            DrawText(
+                buffer,
+                tickPos.x - 0.5f * textWidth,
+                tickPos.y - textHeight - tickSizeX - 1,
+                textHeight,
+                axisNumberColor);
         }
     }
 
-    // Draw Y axis
-    if (isYAxisVisible)
-    {
-        Vector2 a = ToVector2((Point){0.0, ViewState.viewPlaneRect.fr.y});
-        Vector2 b = ToVector2((Point){0.0, ViewState.viewPlaneRect.to.y});
+    if (isYAxisVisible) { // Draw Y axis
+        Vector2 a = ToVector2((Point){0.0, viewRect.fr.y});
+        Vector2 b = ToVector2((Point){0.0, viewRect.to.y});
         DrawLineV(a, b, axisColor);
-
-        float firstTick = ceilf(ViewState.viewPlaneRect.fr.y / spacingY) * spacingY;
-        for (float tick = firstTick; tick <= ViewState.viewPlaneRect.to.y; tick += spacingY)
-        {
-            if (Compare(tick, 0.0f) == 0)
-                continue;
-
+        float firstTick = ceilf(viewRect.fr.y / spacingY) * spacingY;
+        for (float tick = firstTick; tick <= viewRect.to.y; tick += spacingY) {
+            if (Compare(tick, 0.0f) == 0) continue;
+            // Draw the tick
             Vector2 tickPos = ToVector2((Point){0.0, tick});
-            DrawLine(tickPos.x - tickSizeY, tickPos.y, tickPos.x + tickSizeY, tickPos.y, axisColor); // Draw the tick
-            snprintf(buffer, sizeof(buffer), "%.0f", tick);
+            DrawLine(
+                tickPos.x - tickSizeY, tickPos.y,
+                tickPos.x + tickSizeY, tickPos.y,
+                axisColor);
             // Draw the tick number
-            DrawText(buffer, tickPos.x + tickSizeY + 1, tickPos.y - textHeight / 2.0f, textHeight, axisNumberColor);
+            snprintf(buffer, sizeof(buffer), "%.0f", tick);
+            DrawText(
+                buffer,
+                tickPos.x + tickSizeY + 1,
+                tickPos.y - textHeight / 2.0f,
+                textHeight,
+                axisNumberColor);
         }
     }
 }
 
-void RenderLatticePointCloseToMouse() {
-    Point point = ToPoint(GetMousePosition());
+bool TryGetFocusedPoint(Point* outPoint)
+{
+    Point mpos = ToPoint(GetMousePosition());
+    Point closestPoint;
+    double closestDistance = INFINITY;
 
+    double dx, dy, d;
+
+    // check lattice point nearest to the mouse position
     Point latticePoint;
-    latticePoint.x = roundf(point.x);
-    latticePoint.y = roundf(point.y);
+    latticePoint.x = round(mpos.x);
+    latticePoint.y = round(mpos.y);
 
-    double dx = 1000.0 * fabs((latticePoint.x - point.x) * XScreenToPlaneRatio()) / WindowState.windowWidth;
-    double dy = 1000.0 * fabs((latticePoint.y - point.y) * YScreenToPlaneRatio()) / WindowState.windowHeight;
-    double distance = hypotf(dx, dy); // distance in pixels if the screen was 1000 pixels wide/high
-    if (distance < 8.0)
+    dx = fabs((latticePoint.x - mpos.x) * XScreenToPlaneRatio())
+            / WindowState.windowWidth;
+    dy = fabs((latticePoint.y - mpos.y) * YScreenToPlaneRatio())
+            / WindowState.windowHeight;
+    d = hypotf(dx, dy);
+    closestDistance = d;
+    closestPoint = latticePoint;
+    
+    // check all points in core state
+    size_t nPoints = ListSize(CoreState.points);
+    for (size_t i = 0; i < nPoints; i++) {
+        Point p = CoreState.points[i];
+        dx = fabs((p.x - mpos.x) * XScreenToPlaneRatio())
+                / WindowState.windowWidth;
+        dy = fabs((p.y - mpos.y) * YScreenToPlaneRatio())
+                / WindowState.windowHeight;
+        d = hypot(dx, dy);
+        if (d < closestDistance) {
+            closestPoint = p;
+            closestDistance = d;
+        }
+    }
+
+    if (closestDistance > 0.008) {
+        return false;
+    }
+    *outPoint = closestPoint;
+    return true;
+
+}
+
+void RenderFocusedPoint()
+{
+    Point point;
+    if (TryGetFocusedPoint(&point))
     {
-        Vector2 screenPos = ToVector2(latticePoint);
+        Vector2 screenPos = ToVector2(point);
         DrawCircleV(screenPos, 5.0f, BLUE);
     }
 }
@@ -288,7 +447,6 @@ void RenderCoreState()
         LineSegment seg = CoreState.segments[i];
         Point a = CoreState.points[seg.a];
         Point b = CoreState.points[seg.b];
-        Debug("Drawing segment from (%f, %f) to (%f, %f)", a.x, a.y, b.x, b.y);
         DrawLineV(ToVector2(a), ToVector2(b), RED);
     }
 }
@@ -305,8 +463,9 @@ void Initialize()
     ViewState.viewPlaneRect.fr = (Vector2){-10.0, -10.0};
     ViewState.viewPlaneRect.to = (Vector2){20.0, 20.0};
 
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(WindowState.windowWidth, WindowState.windowHeight, "Computer Assisted 2D Geometry");
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_MSAA_4X_HINT);
+    const char* title = "Geometry fun!";
+    InitWindow(WindowState.windowWidth, WindowState.windowHeight, title);
     SetWindowMinSize(400, 400);
 
     SetTargetFPS(60);
@@ -339,7 +498,8 @@ void Draw()
     ClearBackground(RAYWHITE);
     RenderCartesianPlaneAxes();
     RenderCoreState();
-    RenderLatticePointCloseToMouse();
+    RenderFocusedPoint();
+    DrawFPS(5, 5);
     EndDrawing();
 }
 
@@ -356,8 +516,7 @@ int main(void)
 {
     Initialize();
 
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()) {
         Update();
         Draw();
         GatherMetrics();
